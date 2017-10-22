5be07f1bc0c2da074ccdc9b3de3a0d5e23facf04
Christian Daniel Axell <Chris.axell@gmail.com>
2017-10-22 04:47:52 +0200

    ok

---
+++ Oppgave 9/src/sample/ArbTaker.java
@@ -0,0 +1,51 @@
+package sample;
+
+public class ArbTaker {
+    personalia;
+    final int arbtakernr;
+    final int ansettelsesaar;
+    final double maanedslonn;
+    final double skatteprosent;
+
+    public int getArbtakernr() {
+        return arbtakernr;
+    }
+
+    public int getAnsettelsesaar() {
+        return ansettelsesaar;
+    }
+
+    public double getMaanedslonn() {
+        return maanedslonn;
+    }
+
+    public double getSkatteprosent() {
+        return skatteprosent;
+    }
+
+    public void setSkatteprosent(double skatt) {
+        skatteprosent = skatt;
+    }
+
+    public void setMaanedslonn(double lonn) {
+        maanedslonn = lonn;
+
+    }
+
+    public double skatteTrekk() {
+        return getMaanedslonn()*getSkatteprosent();
+    }
+
+    public double bruttoLonn() {
+        return getMaanedslonn()-skatteTrekk()*12;
+    }
+
+    public double skatteTrekkAar() {
+        return skatteTrekk()*10.5;
+    }
+
+    public String navnPaaFormen() {
+        return
+    }
+
+}

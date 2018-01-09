eace25f0d637ae7bad32259fc9b34c5ec2266867
Christian Daniel Axell <Chris.axell@gmail.com>
2018-01-09 18:45:13 +0100

    c

---
+++ untitled/src/sample/Main.java
@@ -0,0 +1,13 @@
+class Test {
+    public int testTall(int tall) {
+        return tall++;
+    }
+}
+
+class nigger {
+    public static void main(String args[]){
+        int tall = 7;
+        tall = Test.testTall(7);
+        System.out.println("svaret er: " + tall);
+    }
+}
